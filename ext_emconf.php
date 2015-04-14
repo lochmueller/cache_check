3264b90f16361a9bb9f1f1a73d78214f3a64471c
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-04-14 14:28:14 +0200

    Fix version for publish the extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,18 +7,17 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title'          => 'Cache-Check',
-	'description'    => 'Cache-Check. Provides functionality to analyze and compare installed caches.',
+	'description'    => 'Check the caches of the TYPO3 caching framework. Provides functionality to analyze and compare installed caches.',
 	'category'       => 'module',
 	'version'        => '0.0.1',
-	'shy'            => 1,
-	'dependencies'   => '',
+	'shy'            => 0,
 	'state'          => 'alpha',
-	'author'         => 'Julian Seitz',
-	'author_email'   => 'julian.seitz@hdnet.de',
+	'author'         => 'Julian Seitz, Tim Lochmüller',
+	'author_email'   => 'julian.seitz@hdnet.de, tim.lochmueller@hdnet.de',
 	'author_company' => 'hdnet.de',
 	'constraints'    => array(
 		'depends'   => array(
-			'typo3' => '6.2.4-0.0.0',
+			'typo3' => '6.2.0-6.2.99..0.0',
 		),
 		'conflicts' => array(),
 		'suggests'  => array(),
