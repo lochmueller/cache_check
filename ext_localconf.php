e641a25345b38e1cb411f780f4a05195595b799a
Julian Seitz <julian.seitz@hdnet.de>
2015-04-16 16:58:54 +0200

    Autoloader aktualisiert.

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,8 +9,15 @@ if (!defined("TYPO3_MODE")) {
 	die ("Access denied.");
 }
 
+$loader = array(
+	'SmartObjects',
+	'ContextSensitiveHelps',
+	'TypeConverter',
+);
+
+
 // Autoloader
-\HDNET\Autoloader\Loader::extLocalconf('HDNET', 'cache_check');
+\HDNET\Autoloader\Loader::extLocalconf('HDNET', 'cache_check', $loader);
 
 /** @var \HDNET\CacheCheck\Service\CacheRegistry $cacheRegistry */
 $cacheRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('HDNET\\CacheCheck\\Service\\CacheRegistry');
