a9f656eef23d47a2d5a291d3dcf74ff213968854
Julian Seitz <julian.seitz@hdnet.de>
2015-04-27 16:19:15 +0200

    getExpires method for FileBackend

--- Classes/Service/Statistics/FileBackend.php
+++ Classes/Service/Statistics/FileBackend.php
@@ -9,6 +9,7 @@
 namespace HDNET\CacheCheck\Service\Statistics;
 
 use HDNET\CacheCheck\Domain\Model\Cache;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Stats service for file backend
@@ -26,5 +27,26 @@ class FileBackend extends SimpleFileBackend {
 	 */
 	public function countTags(Cache $cache) {
 		return 'Not implement yet';
+	}
+
+	/**
+	 * Returns the average left lifetime of the cache entries
+	 *
+	 * @param Cache $cache
+	 *
+	 * @return int|null
+	 */
+	public function getExpires(Cache $cache) {
+		$cacheFileNames = glob(GeneralUtility::getFileAbsFileName($this->getCacheDirectory($cache) . '*'));
+		$expireTimes = array();
+		foreach ($cacheFileNames as $cacheFileName) {
+			$index = (int)file_get_contents($cacheFileName, NULL, NULL, (filesize($cacheFileName) - self::DATASIZE_DIGITS), self::DATASIZE_DIGITS);
+			$expiryTime = (int)file_get_contents($cacheFileName, NULL, NULL, $index, self::EXPIRYTIME_LENGTH);
+			$expireTimes[] = $expiryTime;
+		}
+		if ($expireTimes) {
+			return array_sum($expireTimes) / count($expireTimes);
+		}
+		return NULL;
 	}
 }
